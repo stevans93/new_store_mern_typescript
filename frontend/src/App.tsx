503164f9eb9556941cabe9b9c3b7a84de016522b
stevans93 <stevans93@gmail.com>
2024-02-23 23:59:36 +0100

    new

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           {sampleProducts.map((product) => (
             <li key={product.slug}>
               <img src={product.image} alt={product.name} className="product-image" />
-              <h2>{product.name}</h2> 
+              <h2>{product.name}</h2>
               <p>${product.price}</p>
             </li>
             ))}
