6d97b32045a6fa4711172c9d2ad1ed91762d69d1
stevans93 <stevans93@gmail.com>
2024-02-23 23:39:08 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <header>TS Store</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </>
+  )
+}
+
+export default App
