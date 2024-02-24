76cd6648bd67bdd6da79117d25f213c4882887f8
stevans93 <stevans93@gmail.com>
2024-02-24 17:01:20 +0100

    backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.tsx'
 import './index.css'
 import HomePage from './pages/HomePage.tsx';
+import ProductPage from './pages/ProductPage.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomePage />} />
+      <Route path='product/:slug' element={<ProductPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       
     </Route>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import { sampleProducts } from '../data'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <Row>
         {sampleProducts.map((product) => (
             <Col key={product.slug} sm={6} md={6} lg={3}>
-                <img src={product.image} alt={product.name} className="product-image" />
-                <h2>{product.name}</h2>
+                <Link to={'/product/' + product.slug} className='link-offset-2 link-underline link-underline-opacity-0 link-dark' >
+                    <img src={product.image} alt={product.name} className="product-image" />
+                    <h2>{product.name}</h2>
+                </Link>
                 <p>${product.price}</p>
             </Col>
         ))}
